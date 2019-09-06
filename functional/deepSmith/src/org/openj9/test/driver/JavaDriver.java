e6303c77a510dd747c9755eee78d2adeb38619bb
Longyu Zhang <longyu.zhang@ibm.com>
2019-09-05 20:55:52 -0400

    added tests data -data
    Signed-off-by: Longyu Zhang <longyu.zhang@ibm.com>

--- functional/deepSmith/src/org/openj9/test/driver/JavaDriver.java
+++ functional/deepSmith/src/org/openj9/test/driver/JavaDriver.java
@@ -21,16 +21,54 @@
  *******************************************************************************/
 package org.openj9.test.driver;
 
-// import org.testng.AssertJUnit;
-// import org.testng.annotations.Test;
-// import org.testng.log4testng.Logger;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.StringWriter;
+import java.lang.reflect.Array;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URL;
+import java.net.URLClassLoader;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+import java.util.logging.Logger;
+import javax.tools.JavaCompiler;
+import javax.tools.JavaFileObject;
+import javax.tools.SimpleJavaFileObject;
+import javax.tools.ToolProvider;
 
-// @Test(groups={ "level.extended" })
 public class JavaDriver {
 
 	// private static Logger logger = Logger.getLogger(JavaDriver.class);
 	public static void main(String[] args) throws Throwable {
 		System.out.println("Excellent Job: Running successfully!");
+		File directory = new File(System.getProperty("user.dir"));
+		String dataPath = directory.getParentFile().getParent() + "/functional/deepSmith/data";
+		System.out.println(dataPath);
+		System.out.println(System.getProperty("user.dir"));
+		//previous output is /home/jenkins/workspace/Grinder_Advanced/openjdk-tests/TestConfig/test_output_15677333447999/deepSmith_0
+
+		System.out.println(directory.getCanonicalPath());
+		System.out.println(directory.getAbsolutePath());
+
+		// File testsFolder = new File("/Users/longyuzhang/documents/repos/CasDeepSmith/DSTestTmp");
+		// File[] listofTestFiles = testsFolder.listFiles();
+		// if (listofTestFiles != null) {
+		//   for (File curFile : listofTestFiles) {
+		// 	  System.out.println(curFile.getName());
+		//   }
+		// }
 		// logger.info("running aTestExample: INFO and above level logging enabled");
 		// AssertJUnit.assertEquals(4, 2+2);
 
