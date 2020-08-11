d9e8aec309cc4c367e11a792e588080be8ca75aa
Longyu Zhang <longyu.zhang@ibm.com>
2020-08-11 10:11:20 -0400

    sfwef`:wq
    Signed-off-by: Longyu Zhang <longyu.zhang@ibm.com>

--- external/quarkus_openshift/run_test.sh
+++ external/quarkus_openshift/run_test.sh
@@ -51,9 +51,10 @@ pwd
 
 
 echo "Compile and run quarkus_openshift tests"
-oc login --token=wJ8bbfmvrgbYyfqEVOKbgGtYauUibbZud98aoc2rvJg --server=https://api.ledger.cp.fyre.ibm.com:6443 --insecure-skip-tls-verify
-oc whoami
-oc project
+# s390x linux
+oc login --token=iNHqt959n7P2rWk86uTB0YYYMWzVTLIVnKKfYq0ZA9k --server=https://api.slimy.cp.fyre.ibm.com:6443 --insecure-skip-tls-verify
+# oc whoami
+# oc project
 
 
 # echo "install oc command"
@@ -63,8 +64,14 @@ oc project
 # cp oc /usr/local/bin
 
 
+# mvn -v
+./mvnw -v
 
-./mvnw -pl \!configmap,\!microprofile,\!external-applications/todo-demo-app,\!deployment-strategies/quarkus clean verify -Dquarkus.s2i.base-jvm-image=docker.io/cosmoxiix/adopt11openj9 -Dversion.quarkus=1.6.0.Final -Dts.use-ephemeral-namespaces
+# ./mvnw -pl \!configmap,\!microprofile,\!external-applications/todo-demo-app,\!deployment-strategies/quarkus clean verify -Dquarkus.s2i.base-jvm-image=docker.io/cosmoxiix/adopt11openj9 -Dversion.quarkus=1.6.0.Final -Dts.use-ephemeral-namespaces
+# ./mvnw -pl \!configmap,\!microprofile,\!external-applications/todo-demo-app,\!deployment-strategies/quarkus clean verify -Dversion.quarkus=1.6.0.Final -Dts.use-ephemeral-namespaces
+# ./mvnw -pl \!sql-db/postgresql,\!sql-db/mysql,\!sql-db/mariadb,\!sql-db/mssql,\!security/basic,\!security/jwt,\!security/keycloak,\!security/keycloak-authz,\!security/https-1way,\!security/https-2way,\!http,\!messaging/artemis,\!messaging/artemis-jta,\!messaging/amqp-reactive,\!messaging/qpid,\!scaling,\!configmap,\!microprofile,\!external-applications/todo-demo-app,\!deployment-strategies/quarkus clean verify -Dquarkus.s2i.base-jvm-image=docker.io/cosmoxiix/adopt11openj9 -Dversion.quarkus=1.6.0.Final -Dts.use-ephemeral-namespaces
+# ./mvnw -pl \!sql-db/postgresql,\!sql-db/mysql,\!sql-db/mariadb,\!sql-db/mssql,\!security/basic,\!security/jwt,\!security/keycloak,\!security/keycloak-authz,\!security/https-1way,\!security/https-2way,\!messaging/artemis,\!messaging/artemis-jta,\!messaging/amqp-reactive,\!messaging/qpid,\!scaling,\!microprofile,\!external-applications/todo-demo-app,\!deployment-strategies/quarkus clean verify -Dquarkus.s2i.base-jvm-image=http://registry.access.redhat.com/openjdk/openjdk-11-rhel7/ubi8/openjdk-11/5dd6a4b45a13461646f677f4?architecture=s390x -Dversion.quarkus=1.7.0.Final -Dts.use-ephemeral-namespaces
+./mvnw -pl \!configmap/file-system,\!config-secret/file-system,\!messaging/artemis,\!messaging/artemis-jta,\!messaging/amqp-reactive,\!messaging/qpid,\!sql-db/postgresql,\!sql-db/mssql,\!security/keycloak,\!security/keycloak-authz,\!microprofile,\!external-applications/todo-demo-app,\!deployment-strategies/quarkus clean verify -Dquarkus.s2i.base-jvm-image=docker.io/cosmoxiix/adopt11openj9s390x -Dversion.quarkus=1.7.2.Final -Dts.use-ephemeral-namespaces
 test_exit_code=$?
 echo "Build quarkus_openshift completed"
 
